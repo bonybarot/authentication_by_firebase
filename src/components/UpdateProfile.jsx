1b05f79245847c1933a4e1fc3667c3c5c5989ac7
bonybarot <barotbony29@gmail.com>
2024-05-19 21:48:03 +0530

    authentication by firebase

---
+++ src/components/UpdateProfile.jsx
@@ -0,0 +1,104 @@
+import React, { useRef, useState } from 'react'
+import {Form ,Button , Card, Container, Alert} from "react-bootstrap"
+import {  useAuth } from '../contexts/Authcontext';
+import {Link, Navigate, useNavigate} from "react-router-dom"
+
+
+const Updateprofile = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordconfirmRef = useRef();
+  const {currentuser,UpdateEmail,updatePass } = useAuth()
+  const [err, seterr] = useState("")
+  const [message, setmessage] = useState("")
+  const [loading, setloading] = useState(false)
+  const navigate = useNavigate();
+
+   function handlesubmit(e){
+    e.preventDefault();
+    if(passwordRef.current.value !== passwordconfirmRef.current.value){
+      return seterr("passwords don't match")
+    }
+    
+    const promises = [];
+   
+    if(emailRef.current.value !== currentuser.email){
+        promises.push(UpdateEmail(emailRef.current.value))
+        // console.log(emailRef.current.value,currentuser.email)
+    }
+     if(passwordRef.current.value !== ""){
+      promises.push(updatePass(passwordRef.current.value))
+    }
+    Promise.all(promises).then(()=>{
+      navigate("/")
+      alert("profile updated successfully")
+      window.location.reload()
+    }).catch((er)=>{
+      console.log(er)
+        seterr("failed to update")
+    }).finally(()=>{
+        setloading(false)
+    })
+
+    
+    
+
+  }
+  return (
+    <>
+   
+    <Container
+    className='d-flex align-items-center justify-content-center'
+    style={{minHeight: "100vh"}}
+    >
+      <div className='w-100' style={{minWidth:"400px"}}>
+
+      <Card>
+        <Card.Body>
+          <h2 className=''>Update Profile</h2>
+          {err && <Alert variant='danger'>{err}</Alert>}
+          {message && <Alert variant='success'>{message}</Alert>}
+            <Form onSubmit={handlesubmit}>
+              <Form.Group id="email">
+                  <Form.Label>
+                    Email
+                  </Form.Label>
+                  <Form.Control type="email" ref={emailRef} required 
+                  defaultValue={currentuser.email}
+                  />
+                </Form.Group>
+              <Form.Group id="password">
+                  <Form.Label>
+                    password
+                  </Form.Label>
+                  <Form.Control type="password" ref={passwordRef} 
+                  placeholder="Leave blank to keep the same"
+                  />
+                </Form.Group>
+              <Form.Group id="passwordconfirm">
+                  <Form.Label>
+                  passwordconfirm
+                  </Form.Label>
+                  <Form.Control type="password" ref={passwordconfirmRef}  
+                  placeholder="Leave blank to keep the same"/>
+                  
+                </Form.Group>
+                <br />
+                <Button disabled={loading} type="submit" className="w-100">
+                  Update
+                </Button>
+            </Form>
+          
+        </Card.Body>
+      </Card>
+      <div className='text-center'>
+        <Link to = "/loginpage">Cancel</Link>
+      </div>
+      </div>
+    </Container>
+  
+    </>
+  );
+};
+
+export default Updateprofile;
